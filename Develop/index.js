80bf1da3fa3b89095f49a04789bbad607c7a08a7
Grace Marcoux <gmmarcoux@me.com>
2022-04-24 16:41:43 -0500

    most of index.js completed

--- Develop/index.js
+++ Develop/index.js
@@ -6,126 +6,130 @@ const generateMarkdown = require('./src/generateMarkdown.js');
 const util = require('util'); 
 
 // TODO: Create an array of questions for user input
-const questions = [{
+const questions = [
 
     //1. Name of project
-    type: '',
-    name: '',
-    message: '',
-    validate: txt => {
-        if () => {
-            return true;
-        } else {
-            console.log('');
-            return false;
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your super cool project?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please type in a name!');
+                return false;
+            }
         }
-    }
+    },
 
     //2. Description 
-    type: '',
-    name: '',
-    message: '',
-    validate: txt => {
-        if () => {
-            return true;
-        } else {
-            console.log('');
-            return false;
+    {
+        type: 'input',
+        name: 'description',
+        message: 'How would you describe your project?',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please provide a small desription of your project!');
+                return false;
+            }
         }
-    }
+    },
 
     //3. Installation Instructions 
-    type: '',
-    name: '',
-    message: '',
-    validate: txt => {
-        if () => {
-            return true;
-        } else {
-            console.log('');
-            return false;
+    {
+        type: 'input',
+        name: 'installationinstructions',
+        message: 'If I want to check out your project, how can I install it?',
+        validate: installationinstructionsInput => {
+            if (installationinstructionsInput) {
+                return true;
+            } else {
+                return false;
+            }
         }
-    }
-
+    },
     //4. Usage Information
-    type: '',
-    name: '',
-    message: '',
-    validate: txt => {
-        if () => {
-            return true;
-        } else {
-            console.log('');
-            return false;
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the use of this project?',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                return false;
+            }
         }
-    }
+    },
 
     //5. Contribution guidelines
-    type: '',
-    name: '',
-    message: '',
-    validate: txt => {
-        if () => {
-            return true;
-        } else {
-            console.log('');
-            return false;
-        }
-    }
+    {
+        type: 'confirm',
+        name: 'contribution',
+        message: 'Is it alright for other developers to contribute to your project?',
+        default: false
+    },
 
     //6. Test Instructions 
-    type: '',
-    name: '',
-    message: '',
-    validate: txt => {
-        if () => {
-            return true;
-        } else {
-            console.log('');
-            return false;
+    {
+        type: 'input',
+        name: 'testinstructions',
+        message: 'How can your project be tested?',
+        validate: testinstructionsInput => {
+            if (testinstructionsInput) {
+                return true;
+            } else {
+                return false;
+            }
         }
-    }
+    },
 
     //7. License 
-    type: '',
-    name: '',
-    message: '',
-    choices: '',
-    validate: txt => {
-        if () => {
-            return true;
-        } else {
-            console.log('');
-            return false;
+    {
+        type: 'ckeckbox',
+        name: 'licensing',
+        message: 'Which license would you like to choose?',
+        choices: ['A', 'B', 'C', 'D'],
+        validate: licensingInput => {
+            if (licensingInput) {
+                return true;
+            } else {
+                return false;
+            }
         }
-    }
+    },
 
     //8. Github user
-    type: '',
-    name: '',
-    message: '',
-    validate: txt => {
-        if () => {
-            return true;
-        } else {
-            console.log('');
-            return false;
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your Github Username?',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                return false;
+            }
         }
-    }
+    },
 
     //9. Email
-    type: '',
-    name: '',
-    message: '',
-    validate: txt => {
-        if () => {
-            return true;
-        } else {
-            console.log('');
-            return false;
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is a good email for people to contact you?',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                return false;
+            }
         }
-    }
-}];
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -137,7 +141,7 @@ function writeToFile(fileName, data) {
             console.log('Readme is updated!');
         }
     }); 
-},
+};
 
 // TODO: Create a function to initialize app
 function init() {
